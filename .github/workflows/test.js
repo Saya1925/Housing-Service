848e41f23d8eaf32eda7919fe79ae8a0129a2aea
Saya1925 <118449897+Saya1925@users.noreply.github.com>
2023-04-18 09:25:09 +1000

    Create test.js for React.js

---
+++ .github/workflows/test.js
@@ -0,0 +1,14 @@
+name: Run JEST tests on push
+on:
+  push:
+    branches:
+      - main
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: Install dependencies
+        run: npm install
+      - name: Run tests
+        run: npm test
