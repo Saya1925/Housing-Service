f81f8acd67b8cea919f44ab713a0ac891998430e
{Saya} <{sayamatsui19@yahoo.com}>
2023-04-22 23:58:39 +1000

    Test

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '\\.(css|less|scss)$': 'identity-obj-proxy'
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest'
-  },
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
 };
+
