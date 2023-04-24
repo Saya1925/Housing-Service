711d13618fe0d4776d145994200572c0cc83e8fc
{Saya} <{sayamatsui19@yahoo.com}>
2023-04-24 23:10:22 +1000

    Test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.js?$": "babel-jest",
-    "^.+\\.css$": '<rootDir>/config/jest/cssTransform.js',
+    
   },
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"]
 };
