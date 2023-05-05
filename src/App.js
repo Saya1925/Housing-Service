5396ef0ceb8aff339f78856db5645c14cabdb85f
JingZ <103038638+stacy3321@users.noreply.github.com>
2023-05-05 11:56:17 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
+import Login from "./components/Login";
 
 function App() {
   return (
