fee80bb255f4e7505cabc397904d56208d7ca817
JingZ <103038638+stacy3321@users.noreply.github.com>
2023-05-05 14:31:42 +1000

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,58 +1,32 @@
-import { useState } from "react";
+import React, { useState } from 'react';
 
-function Login(props) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
-  };
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (!email || !password) {
-      setErrorMessage("Please enter both email and password.");
-    } else {
-      setErrorMessage("");
-      props.onLogin({ email, password });
-      setEmail("");
-      setPassword("");
-    }
-  };
+    // handle login logic here
+  }
 
   return (
-    <div className="login-form">
+    <div className="login">
       <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-        <div className="form-group">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-primary btn-block">
-            Login
-          </button>
-        </div>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" value={username} onChange={handleUsernameChange} />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+
+        <button type="submit">Login</button>
       </form>
     </div>
   );
