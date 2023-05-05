a8df304b9dfb6e0cfb31dd85dec09c6620a2c083
JingZ <103038638+stacy3321@users.noreply.github.com>
2023-05-05 12:26:49 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
+import Membership from "./components/Membership";
+import Professionals from "./components/Professionals";
+import CreateTask from "./components/CreateTask";
+import ViewTask from "./components/ViewTask";
 
 function App() {
   return (
