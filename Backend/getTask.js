02c422c074aa9446d5bcf2047412dab5dca65bc4
kenytcng <130018932+kenytcng@users.noreply.github.com>
2023-05-18 03:08:31 +1000

    ver1 offerlist templateA
    ver1 offerlist templateA

--- Backend/getTask.js
+++ Backend/getTask.js
@@ -21,4 +21,51 @@ router.get('/get-target-task', async (req, res) => {
     }
 });
 
+// Route to retrieve the offerList with targetTaskID
+router.get('/getOfferList', async (req, res) => {
+    try {
+        const { taskID } = req.query;
+        const sql = `
+            SELECT offerList.*, userConcat.userName, ROUND(ratingCalc.rating, 1) AS rating
+            FROM offerList
+            LEFT JOIN (
+                SELECT offerList.offeredBy, CONCAT(user.sname, ' ', user.lname) AS userName
+                FROM offerList
+                JOIN user ON offerList.offeredBy = user.userID
+                WHERE offerList.taskID = ?
+                GROUP BY offerList.offeredBy
+            ) AS userConcat ON offerList.offeredBy = userConcat.offeredBy
+            LEFT JOIN (
+                SELECT offerList.offeredBy, AVG(taskList.rating) AS rating
+                FROM offerList
+                JOIN taskList ON offerList.offeredBy = taskList.doneBy
+                WHERE taskList.status = 'taskComplete' AND offerList.taskID = ?
+                GROUP BY offerList.offeredBy
+            ) AS ratingCalc ON offerList.offeredBy = ratingCalc.offeredBy
+            WHERE offerList.taskID = ? ORDER BY offerOrder DESC;
+        `;
+        const [rows, fields] = await db.query(sql, [taskID, taskID, taskID]);
+        res.send(rows);
+        console.log(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error connecting to the database');
+    }
+});
+
+
+// // Route to retrieve the offerList with targetTaskID
+// router.get('/getOfferList', async (req, res) => {
+//     try {
+//         const { taskID } = req.query;
+//         const sql = 'SELECT * FROM offerList WHERE taskID = ?';
+//         const [rows, fields] = await db.query(sql, [taskID]);
+//         res.send(rows);
+//         console.log(rows);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('Error connecting to the database');
+//     }
+// });
+
 module.exports = router;
