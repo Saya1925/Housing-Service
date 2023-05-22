c133e42cfb333d80030a9795a7934172fc8ab8a0
JunhuiShang <130234394+JunhuiShang@users.noreply.github.com>
2023-05-22 16:50:35 +1000

    Update ServicePublised.js

--- src/ServicePublised.js
+++ src/ServicePublised.js
@@ -2,29 +2,44 @@
 //Similar to Make Offer
 import React from 'react';
 
-const ServicePublised = () => {
+const ServicePublished = () => {
+  const handleBackClick = () => {
+
+    console.log("Back button clicked");
+
+  };
+
+  const handlePublishClick = () => {
+
+    console.log("Publish button clicked");
+
+  };
+
   return (
-    <div 
+    <div
       style={{
-      border: '2px solid yellow',
-      padding: '20px',
-      background: 'url("image URL")', 
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}>
-      <h1>Create a service a request</h1>
-      <p>Request:{} </p>
-      <p>Budget: Category:{}</p>
-      <p>Date:{}</p>
-      <p>Location:{}</p>
-      <p>Special Require:{}</p>
-      <p>Desccription:{}</p>
+        border: '2px solid yellow',
+        padding: '20px',
+        background: 'url("image URL")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <h1>Create a service request</h1>
+      <p>Request: {}</p>
+      <p>Budget: Category: {}</p>
+      <p>Date: {}</p>
+      <p>Location: {}</p>
+      <p>Special Require: {}</p>
+      <p>Desccription: {}</p>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button style={{ marginRight: '10px' }}>Back</button>
-        <button>Publish</button>
+        <button style={{ marginRight: '10px' }} onClick={handleBackClick}>
+          Back
+        </button>
+        <button onClick={handlePublishClick}>Publish</button>
       </div>
     </div>
   );
 };
 
-export default ServicePublised;
+export default ServicePublished;
