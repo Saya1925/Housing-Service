b51effa4d465e55e66878d143d766da5e230c4f2
JunhuiShang <130234394+JunhuiShang@users.noreply.github.com>
2023-05-22 16:42:59 +1000

    Update ServicePublised.js

--- src/ServicePublised.js
+++ src/ServicePublised.js
@@ -3,8 +3,22 @@
 
 import React from 'react';
 
-
 const ServicePublised = () => {
-  };
+  return (
+    <div style={{ border: '2px solid yellow', padding: '20px' }}>
+      <h1>Create a service a request</h1>
+      <p>Request:{} </p>
+      <p>Budget: Category:{}</p>
+      <p>Date:{}</p>
+      <p>Location:{}</p>
+      <p>Special Require:{}</p>
+      <p>Desccription:{}</p>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <button style={{ marginRight: '10px' }}>Back</button>
+        <button>Publish</button>
+      </div>
+    </div>
+  );
+};
 
 export default ServicePublised;
